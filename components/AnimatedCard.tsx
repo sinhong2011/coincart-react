db9b818e35c3342e1f5983b7faa709ac1679d5e6
niskan516 <sinhong2011@gmail.com>
2021-10-31 21:46:54 +0800

    - update props

--- components/AnimatedCard.tsx
+++ components/AnimatedCard.tsx
@@ -6,23 +6,30 @@ interface AnimatedCardProps {
   title: string
   content?: number
   duration?: number
+  style?: React.CSSProperties
 }
 
-const Item = styled(Paper)(({ theme }: { theme: Theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}))
+const Item = styled(Paper)(
+  ({ theme, style }: { theme: Theme; style: React.CSSProperties }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 200,
+    borderRadius: 15,
+    ...style,
+  })
+)
 
 const AnimatedCard: React.FC<AnimatedCardProps> = ({
   title,
   content = 0,
   duration = 1,
+  style = {},
 }) => {
   const theme = useTheme()
   return (
-    <Item theme={theme}>
+    <Item theme={theme} style={style}>
       <div>{title}</div>
       <CountUp end={content} duration={duration} />
     </Item>
